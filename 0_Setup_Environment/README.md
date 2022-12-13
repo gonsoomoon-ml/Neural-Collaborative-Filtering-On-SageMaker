72ca13d90ecf491350a9faecf4d7c3c546608c29
Gonsoomoon <gonsoomoon@gmail.com>
2022-12-13 15:22:30 +0000

    edit setup folder, model, config file

--- 0_Setup_Environment/README.md
+++
@@ -1,8 +0,0 @@
-# 0_Setup_Environment/
-
-
-# 1. 실습 파일 
-
-- 0.0.Setup-Environment.ipynb
-    - 전체 노트북의 가장 시작 파일 입니다. 꼭 실행해주세요.
-    
--- 1_Train/src/config.py
+++ 1_Train/src/config.py
@@ -1,11 +1,9 @@
 # dataset name 
 dataset = 'ml-1m'
-assert dataset in ['ml-1m', 'pinterest-20']
+assert dataset in ['ml-1m', 'TBD']
 
 # model name 
 model = 'NeuMF-end'
-# model = 'NeuMF-pre' # 아래 pth 파일이 없어서 에러 발생
-assert model in ['MLP', 'GMF', 'NeuMF-end', 'NeuMF-pre']
 
 main_path = '../data/'
 
@@ -14,7 +12,3 @@ test_rating = main_path + '{}.test.rating'.format(dataset)
 test_negative = main_path + '{}.test.negative'.format(dataset)
 
 model_path = './models/'
-GMF_model_path = model_path + 'GMF.pth'
-MLP_model_path = model_path + 'MLP.pth'
-NeuMF_model_path = model_path + 'NeuMF.pth'
-gs_NeuMF_model_path = model_path + 'NeuMF-end.pth'
